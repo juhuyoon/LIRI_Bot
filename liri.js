833accaa0ebec89b9046d3bfe61297045dc2add6
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-02-09 15:23:45 -0500

    Finished Product

--- liri.js
+++ liri.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 //Grab the keys needed from keys.js
-var keys = require('./keys.js');
-var request = require('request');
-var twitter = require('twitter');
-var spotify = require('node-spotify-api');
-var fs = require('fs');
+var keys = require('./keys.js'),
+    request = require('request'),
+    twitter = require('twitter'),
+    Spotify = require('node-spotify-api'),
+    fs = require('fs');
 
 //Make argv array
 var cmd = process.argv[2];
@@ -41,77 +41,77 @@ function pullRequest(cmd, searchTerm) {
 //function for Tweet requests
 function showTweets() {
     var client = new twitter(keys.twitter);
-    client.get('statuses/user_timerline', function (err, tweets, response) {
-        if (err) {
-            return console.log(err);
-        } else {
-            for (var i = 0; i < tweets.length; i++) {
-                console.log('')
-                console.log('==============================================================')
-                console.log(' Tweet: ' + tweets[i].text)
-                console.log('')
-                console.log(" Tweet Number: " + (i + 1))
-                console.log('')
-                console.log(' Created: ' + tweets[i].created_at)
-                console.log('==============================================================')
-                console.log('')
-            }
+    client.get('statuses/user_timeline', function (error, tweets, response) {
+        if (error) {
+            return console.log(error)
         }
-    });
+        logResults('\nCommand: ' + cmd + '\n')
+
+        tweets.forEach(function (tweet) {
+            console.log('')
+            console.log('==============================================================')
+            console.log(' Tweet: ' + tweet.text)
+            console.log('==============================================================')
+            console.log('')
+            logResults('- ' + tweet.text + '\n')
+        })
+    })
+}
 
     //function for spotify songs
-    function spotifySong(searchItem) {
+    function spotifySong(searchTerm) {
         var spotify = new Spotify(keys.spotify)
-        var query = searchItem ? searchItem : 'The Sign',
-            trackNum = searchItem ? 0 : 5 //done in order to get song by Ace of Bass
-        var secondArg = searchTerm ? searchTerm : '' //accurately log command line entry
-
-        spotify.search({ type: 'track', query: query }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
+        var preSong = searchTerm ? searchTerm : 'The Sign Ace of Base'
+        spotify.search({
+            'type': 'track',
+            query: preSong
+        }, function (err, data) {
+            if(err) {
+                console.log(err + "\n");
+            } else {
+                var track = data.tracks.items[0]
+                console.log('')
+                console.log('==============================================================')
+                console.log('Artist: ' + track.album.artists[0].name);
+                console.log('Song Name: ' + track.name);
+                console.log('Ext URL: ' + track.external_urls.spotify);
+                console.log('Album Name: ' + track.album.name);
+                console.log('==============================================================')
+                console.log('')
+                logResults('\nCommand: ' + cmd + '\n')
             }
-
-            var track = data.tracks.items[trackNum]
-
-            var album = track.album.name;
-            var artist = track.artists[0].name;
-            var ext_url = track.external_urls.spotify;
-            var song = track.name;
-            var result = '* ' + artist + '\n* ' + song + '\n* ' + ext_url + '\n* ' + album
-                + '\n'
-
-            console.log(result);
-            logResults('\nCommand: ' + command + ' ' + secondArg + '\n' + result)
-        });
+        })
     }
+
+
+
+
     //function for OMDB movies
     function movieData(searchTerm) {
-        var query = 'http://www.omdbapi.com/?t=' + searchTerm + '&plot=short&r=json&tomatoes=true';
-        request(query, function (err, response, body) {
-            if (!err && response.statusCode == 200) {
-                var movieDetails = JSON.parse(body);
-                // if no movie entered use below movieDetails for movie
-                if (movieDetails.Response === 'False') {
-                    myMovieDetails('Avengers');
+        var movie = searchTerm ? searchTerm : "Lord%20of%20the%20Rings"
+        var query = 'https://www.omdbapi.com/?apikey=trilogy&t=' + movie;
+
+        request.get(query, function (err, response, body) {
+            if (err) {
+                    return console.log(err);
                 } else {
+                    body = JSON.parse(body)
                     // sends data to console
-                    console.log('')
                     console.log('==============================================================')
-                    console.log(" Title: " + JSON.parse(body)["Title"]);
-                    console.log(" Release Year: " + JSON.parse(body)["Released"]);
-                    console.log(" IMDB Rating: " + JSON.parse(body)["imdbRating"]);
-                    console.log(" Country: " + JSON.parse(body)["Country"]);
-                    console.log(" Language: " + JSON.parse(body)["Language"]);
-                    console.log(" Plot: " + JSON.parse(body)["Plot"]);
-                    console.log(" Actors: " + JSON.parse(body)["Actors"]);
-                    console.log(" Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
-                    console.log(" Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
+                    console.log(" Title: " + body.Title);
+                    console.log(" Release Year: " + body.Year);
+                    console.log(" IMDB Rating: " + body.Ratings[0].Value);
+                    console.log(" Rotten Tomatoes Rating: " + body.Ratings[1].Value);
+                    console.log(" Country: " + body.Country);
+                    console.log(" Language: " + body.Language);
+                    console.log(" Plot: " + body.Plot);
+                    console.log(" Actors: " + body.Actors);
                     console.log('==============================================================')
                     console.log('')
+                    logResults('\nCommand: ' + cmd + '\n')
                 }
-            }
-        });
-    }
+            })
+        };
 
 
     function doThing() {
@@ -128,7 +128,7 @@ function showTweets() {
         })
     }
 
-    function logReults(text) {
+    function logResults(text) {
         fs.appendFile("log.txt", text, function(err) {
             if (err) {
                 return console.log(err);
@@ -136,6 +136,5 @@ function showTweets() {
                 console.log("updated log file ");
             }
         });
-    }
-};
+    };
 
